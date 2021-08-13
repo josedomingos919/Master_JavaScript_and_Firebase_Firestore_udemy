9509ca04a52ed86759c5f60e8cb7f27c3b5c8934
josedomingos919 <josedomingos919@gmail.com>
2021-08-13 09:57:34 +0100

    Fim, estudando os consoles

---
+++ 01fundamentals/02console/app.js
@@ -0,0 +1,29 @@
+console.log("Hello-word");
+console.log(123);
+console.log(true);
+
+var message = "I'm José Domingos Cassua Ndonge";
+
+console.log(message);
+console.log([1, 2, 3]);
+console.log({
+  name: "José Ndonge",
+  age: 19,
+});
+
+console.table({
+  name: "José Ndonge",
+  age: 19,
+});
+
+console.error("Filed to load!!");
+console.warn("Not too bad!");
+
+//Caculate the time bewteen this operation
+console.time("time");
+console.log("Operation...");
+console.log("Operation...");
+console.log("Operation...");
+console.log("Operation...");
+console.log("Operation...");
+console.timeEnd("time");
