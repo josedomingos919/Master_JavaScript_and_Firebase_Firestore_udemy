3bad6f35d29307cf19691bdfface1dde8a7ff046
josedomingos919 <josedomingos919@gmail.com>
2021-08-14 22:21:00 +0100

    Data Types

---
+++ 01fundamentals/05 Data Types/app.js
@@ -0,0 +1,35 @@
+//Primitive
+//undefined
+let item;
+
+//String
+item = "Hello Word";
+
+//Number
+item = 1;
+
+//Boolean
+item = true;
+
+//Null
+item = null;
+
+//Symble
+
+item = Symbol();
+
+//Refrences Type
+
+//Array
+item = [1, 2, 34, 4, 5];
+
+//Object
+item = {
+  name: "José",
+};
+
+//Date
+item = new Date();
+
+//Data
+console.log("type", typeof item);
