06f15b30c39f6172ca1a0d1ce1071633f4e51efa
josedomingos919 <josedomingos919@gmail.com>
2021-08-14 21:46:21 +0100

    variaveis terminadas

---
+++ 01fundamentals/03variable/app.js
@@ -0,0 +1,21 @@
+var name = "José Ndonge";
+console.log(name);
+
+name = "Helena";
+
+console.log(name);
+
+var message;
+console.log(message);
+message = "New message";
+console.log(message);
+
+//letters, number, $
+//
+
+var $name = "sem nome";
+console.log($name);
+
+var firstName; //CamelCase
+var last_name; //underscore
+var FirstName; //PascalCase
