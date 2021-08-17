7d4452af6d497e2f51581bf64c3444ab2ebbdf51
josedomingos919 <josedomingos919@gmail.com>
2021-08-18 00:42:19 +0100

    finalizando numbers and math

---
+++ 01fundamentals/09 Numbers and math model/app.js
@@ -0,0 +1,35 @@
+const num1 = 8;
+const num2 = 1;
+let val;
+
+//Addition
+val = num1 + num2;
+//Subtraction
+val = num1 - num2;
+
+//Multiplication
+val = num1 * num2;
+
+//Division
+val = num1 / num2;
+
+//Modules
+val = num1 % num2;
+val = Math.PI;
+val = Math.round(9.5);
+val = Math.pow(2, 2);
+val = Math.sqrt(4);
+val = Math.abs(-4);
+val = Math.ceil(9.4);
+val = Math.floor(3.9);
+val = Math.min(3, 5, 1, -4);
+val = Math.max(45, 5, 3);
+val = Math.random();
+
+//Random between 1 to five
+val = Math.floor(Math.random() * 5 + 1);
+
+//Result
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
