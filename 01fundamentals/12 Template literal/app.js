46f781ae7776d6a9ea7df6daf72199d801be9e71
josedomingos919 <josedomingos919@gmail.com>
2021-08-18 01:35:30 +0100

    Terminado

--- 01fundamentals/12 Template literal/app.js
+++ 01fundamentals/12 Template literal/app.js
@@ -1,7 +1,10 @@
 let name = "José";
-let age = 20;
+let age = 90;
 let message;
+message = "Hello I'm " + name + " and I'm " + age + " years old";
 
-message = "";
-
+//template literal
+message = `Template Literal: Hello I'm ${name} and I'm ${age}`;
+message = `It is: ${age > 20 ? "Old" : "New"} `;
+//Bugs
 console.log(message);
